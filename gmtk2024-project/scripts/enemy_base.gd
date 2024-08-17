57cdfac1806ebcdad9e8ec2b4a3ae5c133d2cf93
Tiago Somda <tiago@somda.net>
2024-08-16 23:44:03 -0400

    saving work

---
+++ gmtk2024-project/scripts/enemy_base.gd
@@ -0,0 +1,29 @@
+class_name Enemy
+extends CharacterBody2D
+
+@export_group("Movement")
+@export var detection_radius : float = 10.0
+@export var chase_radius := 200.0
+@export var follow_radius := 25.0
+@export var last_direction := Vector2(0.0, 0.0)
+
+@export_group("Combat")
+@export var hitbox : Hitbox
+@export var health : Health
+
+func _ready():
+	hitbox.damaged.connect(_receive_damage)
+	health.health_changed.connect(_on_health_changed)
+	
+func _receive_damage(attack : Attack):
+	health.damage(attack.damage)
+
+func _on_health_changed(previous_health: float, current_health: float) -> void:
+	#print("prev: ", previous_health, "cur: ", current_health)
+	
+	# in case we take multiple hits that take our life to 0
+	# we just delete the object once
+	if current_health <= 0.0 and previous_health != current_health:
+		# instead of deleting, we can disable and reuse
+		# (i.e. pooling on an enemy manager)
+		queue_free()
