b10a80ee0fcd2caa75b8331444f322d372973bcd
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2024-08-16 19:54:32 -0600

    player coin collision detection

--- gmtk2024-project/scripts/combat/hitbox.gd
+++ gmtk2024-project/scripts/combat/hitbox.gd
@@ -6,3 +6,8 @@ signal damaged(value: Attack)
 
 func damage(attack: Attack):
 	damaged.emit(attack)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("player picked up pellet")
+	pass # Replace with function body.
