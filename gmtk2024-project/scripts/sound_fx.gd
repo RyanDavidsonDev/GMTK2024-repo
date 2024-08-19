976f488e0d050cc78d4b52b0d0a233c1f4bd5477
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2024-08-19 16:47:38 -0600

    sound player polish

--- gmtk2024-project/scripts/sound_fx.gd
+++ gmtk2024-project/scripts/sound_fx.gd
@@ -2,27 +2,29 @@ extends Node
 
 @onready var sound_players = get_children()
 
+var active_players : Array[AudioStreamPlayer]
+
 var sounds = {
 	"hit": [
-		load("res://assets-soundfx/hit 1.mp3"),
-		load("res://assets-soundfx/hit 2.mp3"),
-		load("res://assets-soundfx/hit 3.mp3"),
+		load("res://assets-soundfx/hit 1.wav"),
+		load("res://assets-soundfx/hit 2.wav"),
+		load("res://assets-soundfx/hit 3.wav"),
 	],
 	"ammo": [
-		load("res://assets-soundfx/pick up ammo.mp3"),
+		load("res://assets-soundfx/pick up ammo.wav"),
 	],
 	"shoot": [
-		load("res://assets-soundfx/bullet.mp3"),
+		load("res://assets-soundfx/bullet.wav"),
 	],
 	"explosion": [
-		load("res://assets-soundfx/Tiny_Explosion.mp3"),
-		load("res://assets-soundfx/Explosion 2.mp3"),
+		load("res://assets-soundfx/Tiny_Explosion.wav"),
+		load("res://assets-soundfx/Explosion 2.wav"),
 	],
 	"heal": [
-		load("res://assets-soundfx/Health_Collect.mp3"),
+		load("res://assets-soundfx/Health_Collect.wav"),
 	],
 	"hit_barrel": [
-		load("res://assets-soundfx/Barrel_hit.mp3"),
+		load("res://assets-soundfx/Barrel_hit.wav"),
 	],
 }
 
@@ -36,10 +38,23 @@ func play(sound_name):
 	if sound_to_play == null:
 		return 
 	
-	for sound_player in sound_players:
+	for sound_player : AudioStreamPlayer in sound_players:
 		if sound_player.playing == false:
 			sound_player.stream = sound_to_play
 			sound_player.play()
+			active_players.push_front(sound_player)
+			sound_player.finished.connect(func() :active_players.pop_at(active_players.find(sound_player)))
 			return
-			
+	
+	for sound_player :AudioStreamPlayer in active_players :
+		sound_player.stop()
+		var index = active_players.find(sound_player)
+		active_players.pop_at(index)
+		sound_player.stream = sound_to_play
+		sound_player.play()
+		active_players.push_front(sound_player)
+		sound_player.finished.connect(func() :active_players.pop_at(active_players.find(sound_player)))
+		print("no sound player available, but we played from the oldest active player")
+		return
+		
 	print("no sound player available. Too many sounds playing.")
