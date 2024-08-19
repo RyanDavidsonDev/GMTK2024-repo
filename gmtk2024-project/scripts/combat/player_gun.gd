8538233140b855d6b486e5be7babd3140d00f53c
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2024-08-18 22:01:45 -0600

    added lower ammo limit

--- gmtk2024-project/scripts/combat/player_gun.gd
+++ gmtk2024-project/scripts/combat/player_gun.gd
@@ -2,6 +2,7 @@ class_name PlayerGun
 extends AnimatedSprite2D
 
 @export var firing_point : Node2D
+@onready var player: Player = $".."
 
 signal shoot(position: Vector2, direction: Vector2)
 
@@ -16,14 +17,25 @@ func _process(delta):
 	_recoiling(delta)
 	
 func _shooting():
+	
 	var mouse_position = get_global_mouse_position()
 	var mouse_direction = global_position.direction_to(mouse_position)
 	global_rotation = mouse_direction.angle()
 	
 	if Input.is_action_just_pressed("shoot"):
-		SoundFx.play("shoot")
-		shoot.emit(firing_point.global_position, mouse_direction)
-		recoil()
+		
+		if(!is_equal_approx(player.current_size, player.size_floor)):
+			print("size" + str(player.current_size) + "other size" + str(player.size_floor))
+			SoundFx.play("shoot")
+			shoot.emit(firing_point.global_position, mouse_direction)
+			recoil()
+		else :
+			if (player.current_size - player.size_dec < player.size_floor):
+				print("too small little baby man")
+				return
+			else :
+				player.current_size = player.size_floor
+				return
 		
 func _recoiling(delta):
 	if !is_equal_approx(base_offset_x, offset.x):
