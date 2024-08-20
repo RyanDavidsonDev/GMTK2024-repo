6b990a2b68c99c325de054cbd8a6a777003a9871
Tiago Somda <tiago@somda.net>
2024-08-19 22:34:31 -0400

    fixed health bar animation

--- gmtk2024-project/scripts/ui/hud_screen.gd
+++ gmtk2024-project/scripts/ui/hud_screen.gd
@@ -8,109 +8,138 @@ extends BaseScreen
 @export var health_medium_color : Color
 @export var health_large_color : Color
 
-var change_animation_speed = 10.0
-
 var player : Player
-var player_health_floor = 0.0
-var player_health_cap = 0.0
-var player_health_max : float = 0.0
-var player_health_current : float = 0.0
 
 var viewport_size
 var min_health_bar_window_size = 0.0
 var max_health_bar_window_size = 0.0
 
-var target_player_health_current : float = 0
-var target_player_health_max : float = 0
-
-var health_bar_color : Color
-var target_health_bar_color : Color
 var base_color : Color
+var is_in_danger := false
+var music_level := Music.SMALL_LEVEL
+var health_bar_color : Color
+
+# tweens
+var health_bar_size_tween : Tween
+var health_current_value_tween : Tween
+var health_max_value_tween : Tween
+var health_bar_label_tween : Tween
+var health_bar_progress_color : Tween
+var anim_duration = 0.75 # in seconds
 
 func _ready():
 	super._ready()
-	GameEvents.on_player_health_changed.connect(_update_targets)
-	GameEvents.on_music_level_changed.connect(_on_music_level_changed)
 	
 	base_color = health_normal_color
-	target_health_bar_color = base_color;
-	
+		
 	_set_player()
 	_on_music_level_changed()
 	_update_targets()
 	
 	_on_viewport_size_changed()
+	
 	get_viewport().size_changed.connect(_on_viewport_size_changed)
-	_update_health_components()
-	
-func _process(_delta:float):
-	if !is_equal_approx(target_player_health_current,player_health_current):
-		var direction = sign(target_player_health_current - player_health_current)
-		player_health_current += direction*_delta*change_animation_speed
-		if is_zero_approx(int(target_player_health_current) - int(player_health_current)):
-			target_player_health_current = player_health_current
-		_update_health_components()
-	
-	if !is_equal_approx(target_player_health_max, player_health_max):
-		var direction = sign(target_player_health_max - player_health_max)
-		player_health_max += direction*_delta*change_animation_speed
-		if is_zero_approx(int(target_player_health_max) - int(player_health_max)):
-			target_player_health_max = player_health_max
-		_update_health_components()
-		
-	if !is_same(target_health_bar_color, health_bar_color):
-		health_bar_color = health_bar_color.lerp(target_health_bar_color, _delta*change_animation_speed)
-		if health_bar_color.is_equal_approx(target_health_bar_color):
-			health_bar_color = target_health_bar_color
-			_update_health_components()
+	GameEvents.on_player_health_changed.connect(_update_targets)
+	GameEvents.on_music_level_changed.connect(_on_music_level_changed)
 
-func _update_health_components():
-	
-	var health_bar_size_percentage = inverse_lerp(
-		player_health_cap,
-		player_health_floor,
-		player_health_max)
-	
-	var health_bar_window_size = lerp(
-		min_health_bar_window_size,
-		max_health_bar_window_size,
-		health_bar_size_percentage) #hard coding this as I don't think we need the size of the  bar to change
-	
-	health_bar.size.x = health_bar_window_size
-	health_bar.max_value = player_health_max
-	health_bar.value = player_health_current
-	
-	#print(" ---- current health" + str(player_health_current))
-	#health_label.text = str(snapped(player_health_current, .01)) + " / " + str(int(ceil(player_health_max)))
-	health_bar_label.text = "%.2f / %.2f" % [player_health_current, player_health_max]
-	
-	health_bar.tint_progress = health_bar_color
+func _set_player():
+	player = get_tree().get_first_node_in_group("player")
+	return player != null
 
 func _update_targets():
+	_update_health_bar_size()
+	_update_health_bar_values()
+	_update_health_label()
+	_update_health_bar_color()
+
+func _update_health_label():
+	if player == null:
+		if _set_player() == false:
+			return
+			
+	health_bar_label.text = "%.2f / %.2f" % [player.health.current_health, player.health.max_health]
+
+func _update_health_bar_color():
+	
+	var target_color = base_color
+	if is_in_danger:
+		target_color = health_danger_color
+	
+	if health_bar.tint_progress.is_equal_approx(target_color):
+		health_bar.tint_progress = target_color
+	else:
+		health_bar_progress_color = _animate(
+			health_bar_progress_color,
+			health_bar,
+			"tint_progress",
+			target_color)
+
+func _update_health_bar_values():
 	if player == null:
 		if _set_player() == false:
 			return
 	
-	player_health_floor = float(player.max_health_floor)
-	player_health_cap = float(player.max_health_cap)
+	health_max_value_tween = _animate(
+		health_max_value_tween,
+		health_bar,
+		"max_value",
+		player.health.max_health)
+	
+	health_current_value_tween = _animate(
+		health_current_value_tween,
+		health_bar,
+		"value",
+		player.health.current_health)
+		
+	is_in_danger = player.health.current_health < 9.9
 
-	target_player_health_max = player.health.max_health
-	target_player_health_current = player.health.current_health
+func _update_health_bar_size():
+	if player == null:
+		if _set_player() == false:
+			return
+
+	var health_bar_size_percentage = inverse_lerp(
+		player.max_health_cap,
+		player.max_health_floor,
+		player.health.max_health)
 	
-	if target_player_health_current <= 9.9:
-		target_health_bar_color = health_danger_color
-	else:
-		target_health_bar_color = base_color
+	var health_bar_window_size = lerp(
+		min_health_bar_window_size,
+		max_health_bar_window_size,
+		health_bar_size_percentage) 
 	
-func _set_player():
-	player = get_tree().get_first_node_in_group("player")
-	return player != null
+	health_bar_size_tween = _animate(
+		health_bar_size_tween,
+		health_bar,
+		"size:x",
+		health_bar_window_size)
+
+func _animate(
+	tween: Tween,
+	object: Object,
+	property: NodePath,
+	final_val: Variant) -> Tween:
+	
+	if tween and (tween.is_valid() or tween.is_running()):
+		tween.kill()
+		tween = null
+	
+	tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(
+		object,
+		property,
+		final_val,
+		anim_duration)
+
+	return tween
 
 func _on_viewport_size_changed():
 	viewport_size = get_viewport_rect().size
 	min_health_bar_window_size = 120.0
 	max_health_bar_window_size = viewport_size.x * 0.5
-	_update_targets()
+	_update_health_bar_size()
 
 func _on_music_level_changed():
 	match Music.get_level():
