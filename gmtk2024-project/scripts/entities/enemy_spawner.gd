a27a98e577f65bc143bf4e271809560353f36734
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2024-08-20 03:46:41 -0600

    finalized spawn rates

--- gmtk2024-project/scripts/entities/enemy_spawner.gd
+++ gmtk2024-project/scripts/entities/enemy_spawner.gd
@@ -6,15 +6,24 @@ extends RadialSpawner
 var timer_current = 5
 
 @export_subgroup("spawn_num range")
-@export var spawn_num_min: float = 3
-@export var spawn_num_max: float = 10
-var spawn_num_current = 3
+@export var spawn_num_min: float = 1
+@export var spawn_num_max_floor: float = 2
+var spawn_num_max_curr: float = spawn_num_max_floor
+@export var spawn_num_max_cap: float = 50
+
+var spawn_num_current = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
+	GameEvents.on_player_size_changed.connect(update_spawn)
 
+func update_spawn(curr :float, max: float):
+	spawn_num_max_curr = lerp(spawn_num_max_floor, spawn_num_max_cap, 
+		inverse_lerp(1, max, curr))
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
@@ -23,18 +32,21 @@ func _ready() -> void:
 
 
 func spawn():
-	var random_direction = randi_range(0, 360)
-	var min_dist = spawn_min_distance
-	var max_dist = min_dist + randi_range(0, int(min_dist)) * spawn_min_range_multiplier
-	var random_distance = randi_range(min_dist, max_dist)
-	var spawn_position = player.global_position
-	
-	# Calculate the spawn position at random distance in the random direction
-	spawn_position.x += random_distance * cos(deg_to_rad(random_direction))
-	spawn_position.y += random_distance * sin(deg_to_rad(random_direction))
-	
-	
-	spawn_at_location(spawn_position)
+	spawn_num_current = randi_range(spawn_num_max_floor, spawn_num_max_curr)
+	for i in spawn_num_current:
+		var random_direction = randi_range(0, 360)
+		var min_dist = spawn_min_distance
+		var max_dist = min_dist + randi_range(0, int(min_dist)) * spawn_min_range_multiplier
+		var random_distance = randi_range(min_dist, max_dist)
+		var spawn_position = player.global_position
+		
+		# Calculate the spawn position at random distance in the random direction
+		spawn_position.x += random_distance * cos(deg_to_rad(random_direction))
+		spawn_position.y += random_distance * sin(deg_to_rad(random_direction))
+		
+		
+		
+		spawn_at_location(spawn_position)
 
 
 
