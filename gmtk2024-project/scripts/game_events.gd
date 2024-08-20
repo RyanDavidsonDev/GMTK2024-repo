b5492f0514e1b386ee127cc9e1943ae4595111f8
Tiago Somda <tiago@somda.net>
2024-08-19 22:35:09 -0400

    fixed game_events warning signals

--- gmtk2024-project/scripts/game_events.gd
+++ gmtk2024-project/scripts/game_events.gd
@@ -1,9 +1,20 @@
-# warnings-disable
+
 extends Node
 
+@warning_ignore("unused_signal")
 signal on_player_shoot(position: Vector2, direction: Vector2, scale: float)
+
+@warning_ignore("unused_signal")
 signal on_player_died
+
+@warning_ignore("unused_signal")
 signal on_player_health_changed
+
+@warning_ignore("unused_signal")
 signal on_enemy_explode(location:Vector2)
+
+@warning_ignore("unused_signal")
 signal on_music_level_changed
+
+@warning_ignore("unused_signal")
 signal on_supress_player_shoot(increment:int)
